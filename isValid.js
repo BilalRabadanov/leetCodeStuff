9a3a940133de6ac7c3bfcb8dda7694e119d8d815
Bilal <bilalrabadanov1@gmail.com>
2022-12-02 16:15:27 +0300

    Valid Parentheses

---
+++ isValid.js
@@ -0,0 +1,22 @@
+function isValid(s) {
+  let stack = [];
+  let openSet = new Set(["(", "{", "["]);
+  let closeOpenMap = new Map([
+    [")", "("],
+    ["]", "["],
+    ["}", "{"],
+  ]);
+
+  for (let i = 0; i < s.length; i++) {
+    if (openSet.has(s[i])) {
+      stack.push(s[i]);
+    } else {
+      const pop = stack.pop();
+      if (pop !== closeOpenMap.get(s[i])) {
+        return false;
+      }
+    }
+  }
+  
+  return stack.length === 0;
+}
